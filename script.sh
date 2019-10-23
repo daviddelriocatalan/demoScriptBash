132f35a5158ce029a4c3ead1131c04ea9091489a
DAVID DEL RIO CATALAN <53259208v@AD.FPMISLATA.COM>
2019-10-23 10:36:18 +0200

    Fix(#5):script de bash con ampliacion lorenzo

--- script.sh
+++ script.sh
@@ -8,3 +8,21 @@ git --no-pager log --pretty=tformat:"%s" | grep "^Feat(" | sed "s/^Feat(//g" | s
 echo Fix
 git --no-pager log --pretty=tformat:"%s" | grep "^Fix(" | sed "s/^Fix(//g" | sed "s/):/:/g" | sort -V | uniq
 
+
+
+#echo <html>
+#echo <body>
+#echo <h1>Features</h1>
+#git --no-pager log --pretty=tformat:"%s" | grep "^Feat(" | sed "s/^Feat(//g" | sed "s/):/:/g" | sort -V | uniq | while read line 
+#do
+#echo <p> $line </p>
+#done
+#echo </br> 
+#echo </br>
+#echo <h1>Fix</h1>
+#git --no-pager log --pretty=tformat:"%s" | grep "^Fix(" | sed "s/^Fix(//g" | sed "s/):/:/g" | sort -V | uniq | while read line 
+#do
+#echo <p> $line </p>
+#done
+#echo </body>
+#echo </html>
