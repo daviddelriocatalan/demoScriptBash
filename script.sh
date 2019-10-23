4b48fde7cb2cc2d69a9c3bbb3e159b6e0e890e65
DAVID DEL RIO CATALAN <53259208v@AD.FPMISLATA.COM>
2019-10-23 10:28:36 +0200

    Fix(#4):script de bash acabado

--- script.sh
+++ script.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-git --no-pager log --pretty=tformat:"%H %c"
+
+#git --no-pager log --pretty=tformat:"%s"
+
+echo Features
+git --no-pager log --pretty=tformat:"%s" | grep "^Feat(" | sed "s/^Feat(//g" | sed "s/):/:/g" | sort -V | uniq
+
+echo Fix
+git --no-pager log --pretty=tformat:"%s" | grep "^Fix(" | sed "s/^Fix(//g" | sed "s/):/:/g" | sort -V | uniq
+
